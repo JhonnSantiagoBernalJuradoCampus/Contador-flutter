285c4142972b4e5b7e5513590e90bdc0ade4bf10
jbern <jhon.bernal@pensemos.com>
2024-10-14 20:04:31 -0500

    Conociendo StatefulWidget, agregando estados al momento de onpressed, nuevos botones para reiniciar, restar clicks

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,24 +18,57 @@ class CounterScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text('Counter screen'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '10',
-              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              clickCounter.toString(),
+              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            Text('Clicks',
-            style: TextStyle(fontSize: 25),)
+            Text('Click${(clickCounter == 1 || clickCounter == -1) ? '': 's'}',
+            style: const TextStyle(fontSize: 25),)
           ],
         )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CounterScreen()));
-        },
-        child: const Icon(Icons.plus_one),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 15,
+            ),
+            child: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter= 0;
+              });
+            },
+              child: const Icon(Icons.replay),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 15,
+            ),
+            child: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter++;
+              });
+            },
+              child: const Icon(Icons. plus_one),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter--;
+              });
+            },
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
